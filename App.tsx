451d138d7feb8c89e2fc5914dc7e69dd5613796b
Aarav2390 <95223645+Aarav2390@users.noreply.github.com>
2022-10-20 06:41:48 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import React,{Component} from "react";
+import {View,Text,TouchableOpacity} from "react-native";
+
+export default class App extends Component{
+  render(){
+    return(
+        <Text>Redeem Free Money!</Text>
+
+        <TouchableOpacity>
+          <Text>
+            100 Rs/-
+          </Text>
+        </TouchableOpacity>
+    )
+  }
+}
